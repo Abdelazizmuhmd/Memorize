fc1de7623ac6979acd5adf92abe898918a3c626e
Abdelaziz Mohamed <abdelazizwmuhmd@gmail.com>
2020-07-22 23:39:12 +0200

    Made face and back of the card

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Memorize
 //
-//  Created by Zizo on 7/22/20.
+//  Created by Abdelaziz on 7/22/20.
 //  Copyright © 2020 Abdelazizfmohamed@gmail.com. All rights reserved.
 //
 
@@ -10,9 +10,68 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+    HStack() {
+        ForEach(0..<4) {  index in
+            Cardview(isFaceUP: false)
+        }
     }
+        .font(Font.largeTitle)
+        .padding()
+        .foregroundColor(Color.orange)
+    }
+    
 }
+
+
+struct Cardview: View{
+    var isFaceUP: Bool
+    var body: some View{
+        ZStack(){
+        if isFaceUP{
+           RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+           RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+           Text("👻")
+        }else {
+            RoundedRectangle(cornerRadius: 10.0).fill()
+
+            }
+
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
